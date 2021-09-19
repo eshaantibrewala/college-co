4f5acf304e8f5c477061c23a5c74475801348c52
yashsavalia1 <yashsavalia1@gmail.com>
2021-09-19 01:07:34 -0500

    setup commit 1

---
+++ components/common/navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <nav>
+            <a>Hello</a>
+            <a>Hello</a>
+            <a></a>
+            <a></a>
+        </nav>
+    )
+}
+
+export default Navbar
